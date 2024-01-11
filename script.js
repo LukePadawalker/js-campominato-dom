acc3cf747098051014cea0ba8b85f38618b0c7f3
LukePadawalker <padurariu.luca@gmail.com>
2024-01-11 16:12:24 +0100

    trying to fix the difficulty

--- script.js
+++ script.js
@@ -12,114 +12,96 @@ const selector = document.getElementById('selector');
 
 
 // valori iniziali
-// const rows = 10;
-// const cols = 10;
-// const totalCells = rows * cols;
-let selectorValue = selector.value;
-console.log(selectorValue);
+let rows;
+let cols;
 
+selector.addEventListener('change', function(){
+    
+    let selectorValue = selector.value;
+    console.log(selectorValue);
+    switch(selectorValue){
+        case "medium":
+            rows = 9;
+            cols = 9;
+            break;
+        case "hard":
+            rows = 7;
+            cols = 7;
+            break;
+        default:
+            rows = 10;
+            cols = 10;
+            break;
+                    
+    }
+    console.log(rows);
+    console.log(cols);
+                
+                
+    const numOfRows = document.querySelector(':root');
+    const setnumOfRows = () => {
+        numOfRows.style.setProperty('--cols-per-row', cols);
+        return setnumOfRows;
+                  
+    }
+                
+})
 
 
 // funzioni
+            
+// crea una cella
+
 const createCell = () => {
     const cell = document.createElement('div');
     cell.className = 'cell';
     cell.classList.add("d-flex", "justify-content-center", "align-items-center");
-    
     return cell;
 }
 
 
-function selectorListener(){
-    
-    let rows;
-    let cols;
-    let totalCells;
-    
-    selector.addEventListener('submit', function(event){
-
-        event.preventDefault;
-
-        if (selectorValue == 1){
-            console.log("hai scelto la modalita Facile");
-            rows = 7;
-            cols = 7;
-        }else if(selectorValue == 2){
-            console.log("hai scelto la modalita media");
-            rows = 9;
-            cols = 9;
-        }else if(selectorValue == 3){
-            console.log("hai scelto la modalita difficile");
-            rows = 10;
-            cols = 10;
-        }else{
-            console.log("hai scelto la modalita Estrema");
-            rows = 30;
-            cols = 30;
-        }
-
-        totalCells = rows * cols;
-
-        console.log(rows);
-        console.log(cols);
-        console.log(totalCells);
-    
-    })
-
-
-    return rows;
-    return cols;
-    return totalCells;
-
-
-}
+let totalCells = rows * cols;
 
 // svolgimento
-// selectorListener();
-// definiamo i vari casi in base alla difficolta del gioco
-console.log(selectorValue);
 
 // creiamo una variabile falsa di default
 let gridExist = false;
 
-
 button.addEventListener('click', function(){
 
+    console.log(totalCells);
+
+    for(let i = 0; i < totalCells; i++){
+
+        // variabili
+        const newCell = createCell();        
+
+        // creiamo il contenitore per il numero
+        cellContainer = document.createElement('h5');
 
 
-    if (gridExist == false){
-        for(let i = 0; i < totalCells; i++){
-
-            // variabili
-            const newCell = createCell();        
-
-            // creiamo il contenitore per il numero
-            cellContainer = document.createElement('h5');
+        //inseriamo il numero dentro il contenitore
+        numInCell = i + 1;
+        cellContainer.textContent = numInCell;
 
 
-            //inseriamo il numero dentro il contenitore
-            numInCell = i + 1;
-            cellContainer.textContent = numInCell;
+        // inseriamo il contenitore dentro la cella
+        newCell.appendChild(cellContainer);
+        
+
+        // inseriamo la cella dentro la griglia
+        grid.appendChild(newCell);
+
+        // coloriamo la cella di blu
+        newCell.addEventListener('click', () => {
+
+            newCell.classList.add("clicked");
+            console.log(newCell.innerText);
+
+            // coloriamola di rosso se contiene una bomba
 
 
-            // inseriamo il contenitore dentro la cella
-            newCell.appendChild(cellContainer);
-            
-
-            // inseriamo la cella dentro la griglia
-            grid.appendChild(newCell);
-
-
-            newCell.addEventListener('click', () => {
-                newCell.classList.toggle("clicked");
-                console.log(newCell.innerText);
-            })
-
-
-
-
-
-        }
+        })
 
 
         // facciamo si che il bottone play generi una sola griglia
